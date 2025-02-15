e53b9cf003b73af46cc6fe43c4ed6ecbbdc12c92
Samaksh Arora <samaksharora.09@gmail.com>
2025-02-15 13:06:17 -0500

    Added user-related code.

--- api/index.py
+++ api/index.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.secret_key ='amherstburgers'
+
+users = {
+    'admin': {'password': 'password123'},
+}
+
+@app.route("/api/login", methods= ['GET', 'POST'])
+
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users and users[username]['password'] == [password]:
+            session['username'] = username
+            return f"Welcom to DRINKUP!, {username}", 200
+        else:
+            return "Invalid username or password", 401
+
+    return render_template('login.html')
+@app.route("/api/dashboard")
+
+
